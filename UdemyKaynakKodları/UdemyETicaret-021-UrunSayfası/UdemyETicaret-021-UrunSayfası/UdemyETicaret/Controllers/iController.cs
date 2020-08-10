66234c8a1d6647663adffc7bfb8d51b9f6d4fa70
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2020-08-10 21:11:03 +0300

    NORTWND DB'ye eklendi.

---
+++ UdemyKaynakKodları/UdemyETicaret-021-UrunSayfası/UdemyETicaret-021-UrunSayfası/UdemyETicaret/Controllers/iController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyETicaret.DB;
+using UdemyETicaret.Models.i;
+
+namespace UdemyETicaret.Controllers
+{
+    public class iController : BaseController
+    {
+        // GET: i
+        [HttpGet]
+        public ActionResult Index(int id = 0)
+        {
+            IQueryable<DB.Products> products = context.Products;
+            DB.Categories category = null;
+            if (id > 0)
+            {
+                products = products.Where(x => x.Category_Id == id);
+                category = context.Categories.FirstOrDefault(x => x.Id == id);
+            }
+            var viewModel = new Models.i.IndexModel()
+            {
+                Products = products.ToList(),
+                Category = category
+            };
+            return View(viewModel);
+        }
+        [HttpGet]
+        public ActionResult Product(int id = 0)
+        {
+            var pro = context.Products.FirstOrDefault(x => x.Id == id);
+
+            if (pro == null) return RedirectToAction("index", "i");
+
+            ProductModels model = new ProductModels()
+            {
+                Product = pro,
+                Comments = pro.Comments.ToList()
+            };
+            return View(model);
+        }
+    }
+}
