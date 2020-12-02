b9be9b27d4ae236802ecaccf354bca0a6ebf9bf3
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2020-12-02 21:30:41 +0300

    Yönetim-Ürünleri Listeleme işlemi gerçekleştirildi.

---
+++ SelcukETicaret/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SelcukETicaret.Controllers
+{
+    public class ProductController : BaseController
+    {
+        // GET: Product
+        public ActionResult i()
+        {
+            if (IsLogon() == false) return RedirectToAction("index", "i");
+            else if (((int)(CurentUser().MemberType))<4)
+            {
+                return RedirectToAction("index", "i");
+            }
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
